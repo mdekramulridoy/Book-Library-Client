bd5be56b8d9d8f517a256c2848cdfc54f32870ba
ekramulhoqueridoy <158032414+ekramulhoqueridoy@users.noreply.github.com>
2024-12-24 19:59:11 +0600

    added dynamic title

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import AuthProvider from "./providers/AuthProvider";
-import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import NotFound from "./components/NotFound";
 import PrivateRoute from "./routes/PrivateRoute";
@@ -17,6 +16,8 @@ import BorrowedBooks from "./components/private/BorrowedBooks";
 import DetailsPage from "./components/private/DetailsPage";
 import CategoryPage from "./components/private/CategoryPage";
 import UpdateBook from "./components/private/UpdateBook";
+import Title from "./components/Title";
+
 
 const router = createBrowserRouter([
   {
@@ -25,45 +26,107 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: (
+          <>
+            <Title title="Home" /> 
+            <Home />
+          </>
+        ),
       },
       {
         path: "/category/:category",
-        element: <PrivateRoute><CategoryPage /></PrivateRoute>,
+        element: (
+          <>
+            <Title title="Category" /> 
+            <PrivateRoute>
+              <CategoryPage />
+            </PrivateRoute>
+          </>
+        ),
       },
       {
         path: "login",
-        element: <Login />,
+        element: (
+          <>
+            <Title title="Login" /> 
+            <Login />
+          </>
+        ),
       },
       {
         path: "register",
-        element: <Register />,
+        element: (
+          <>
+            <Title title="Register" /> 
+            <Register />
+          </>
+        ),
       },
       {
         path: "add-book",
-        element: <PrivateRoute><AddBook /></PrivateRoute>,
+        element: (
+          <>
+            <Title title="Add Book" /> 
+            <PrivateRoute>
+              <AddBook />
+            </PrivateRoute>
+          </>
+        ),
       },
       {
         path: "all-books",
-        element: <PrivateRoute><AllBooks /></PrivateRoute>,
+        element: (
+          <>
+            <Title title="All Books" /> 
+            <PrivateRoute>
+              <AllBooks />
+            </PrivateRoute>
+          </>
+        ),
       },
       {
         path: "borrowed-books",
-        element: <PrivateRoute><BorrowedBooks /></PrivateRoute>,
+        element: (
+          <>
+            <Title title="Borrowed Books" /> 
+            <PrivateRoute>
+              <BorrowedBooks />
+            </PrivateRoute>
+          </>
+        ),
       },
       {
         path: "details-books/:id",
-        element: <PrivateRoute><DetailsPage /></PrivateRoute>,
+        element: (
+          <>
+            <Title title="Book Details" /> 
+            <PrivateRoute>
+              <DetailsPage />
+            </PrivateRoute>
+          </>
+        ),
       },
       {
         path: "update-book/:id",
-        element: <PrivateRoute><UpdateBook /></PrivateRoute>,
+        element: (
+          <>
+            <Title title="Update Book" /> 
+            <PrivateRoute>
+              <UpdateBook />
+            </PrivateRoute>
+          </>
+        ),
       },
     ],
   },
   {
     path: "*",
-    element: <NotFound />,
+    element: (
+      <>
+        <Title title="Page Not Found" /> 
+        <NotFound />
+      </>
+    ),
   },
 ]);
 
